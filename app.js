86af70f458cb6e353e36f0ca4d15f2177c0f6785
Corey Daniels <corey@skookum.com>
2013-01-08 22:08:10 -0500

    Now using Jade/Stylus. Dumped Bootstrap for custom styles.

--- app.js
+++ app.js
@@ -2,16 +2,33 @@ var express = require('express'),
         app = express(),
      server = require('http').createServer(app),
          io = require('socket.io').listen(server),
+     stylus = require('stylus'),
+        nib = require('nib'),
        five = require('johnny-five'),
        board , led;
 
-server.listen(8080);
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public',
+    compile: compile
+  }
+));
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.render('index');
 });
+
+server.listen(8080);
 
 board = new five.Board();
 
