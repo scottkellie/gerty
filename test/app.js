1603af6f79825c856502f51085e59bfda682d75d
Corey Daniels <corey@skookum.com>
2013-01-11 16:07:44 -0500

    Reorganized the project

---
+++ test/app.js
@@ -0,0 +1,30 @@
+var gerty = require('../lib/gerty.js'),
+     five = require('johnny-five'),
+     board , led, button;
+
+gerty.createDashboard();
+
+board = new five.Board();
+
+board.on("ready", function() {
+  led = new five.Led(13);
+
+/* Here's how it should work:
+-------------------------------------
+
+  button = new gerty.Button();
+
+  button.on("down", function(){
+    led.toggle();
+  });
+
+-------------------------------------
+*/
+
+  led.on();
+
+  this.wait( 3000, function() {
+    led.off();
+  });
+
+});
