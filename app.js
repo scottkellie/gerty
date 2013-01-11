068bee15b5e61fd733d3ae1c1b1b68d612b907f1
Corey Daniels <corey@skookum.com>
2013-01-10 21:34:06 -0500

    Began work to make Gerty a Node module

--- app.js
+++ app.js
@@ -1,43 +1,30 @@
-var express = require('express'),
-        app = express(),
-     server = require('http').createServer(app),
-         io = require('socket.io').listen(server),
-     stylus = require('stylus'),
-        nib = require('nib'),
-       five = require('johnny-five'),
-       board , led;
+var gerty = require('./node_modules/gerty/gerty.js'),
+     five = require('johnny-five'),
+     board , led, button;
 
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib());
-}
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.logger('dev'))
-app.use(stylus.middleware(
-  { src: __dirname + '/public',
-    compile: compile
-  }
-));
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function (req, res) {
-  res.render('index');
-});
-
-server.listen(8080);
+gerty.createDashboard();
 
 board = new five.Board();
 
 board.on("ready", function() {
   led = new five.Led(13);
 
-  io.sockets.on('connection', function (socket) {
-    socket.on('click', function () {
-      led.toggle();
-    });
-  }); 
+/* Here's how it should work:
+-------------------------------------
+
+  button = new gerty.Button();
+
+  button.on("down", function(){
+    led.toggle();
+  });
+
+-------------------------------------
+*/
+
+  led.on();
+
+  this.wait( 3000, function() {
+    led.off();
+  });
+
 });
